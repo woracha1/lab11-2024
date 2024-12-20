25dd507102e366d30f34eeff2cbbeb33f28a243a
GGTTGTGt <zz814170@gmail.com>
2024-12-20 17:12:41 +0700

    lab11_1 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(){
+    string input ;
+    string grade[] = {"A", "B+", "B", "C+", "C","D+","D", "F","W" };
+    cout << "Press Enter 3 times to reveal your future." ;
+    getline(cin,input);
+    getline(cin,input);
+    getline(cin,input);
+    srand(time(0));
+    input = grade[rand()%9];
+    cout << "You will get "<<input<<" in this 261102." ;
+    
+    return 0 ;
+}
